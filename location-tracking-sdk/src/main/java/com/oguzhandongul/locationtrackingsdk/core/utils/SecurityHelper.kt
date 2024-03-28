d2d98e8578336e39b2846b9001308d20e949999a
oguzhandongul <oguzhandongul@gmail.com>
2024-03-28 21:50:45 +0100

    add comments to helper classes

--- location-tracking-sdk/src/main/java/com/oguzhandongul/locationtrackingsdk/core/utils/SecurityHelper.kt
+++ location-tracking-sdk/src/main/java/com/oguzhandongul/locationtrackingsdk/core/utils/SecurityHelper.kt
@@ -5,9 +5,25 @@ import android.content.SharedPreferences
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKey
 
+/**
+ * Helper object for creating and retrieving secure SharedPreferences instances
+ * using Android's EncryptedSharedPreferences. This provides an additional layer
+ * of security for sensitive data stored on the device.
+ */
+
 internal object SecurityHelper {
 
+    /**
+     *  Name of the SharedPreferences file.
+     */
     private const val PREF_FILENAME = "token_prefs"
+
+    /**
+     * Obtains an EncryptedSharedPreferences instance for secure data storage.
+     *
+     * @param context The application Context.
+     * @return An EncryptedSharedPreferences object ready for secure storage of key-value pairs.
+     */
     fun getSharedPref(context: Context): SharedPreferences {
         val masterKey = MasterKey.Builder(context)
             .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
--- location-tracking-sdk/src/main/java/com/oguzhandongul/locationtrackingsdk/data/remote/models/requests/RefreshTokenRequest.kt
+++
@@ -1,3 +0,0 @@
-package com.oguzhandongul.locationtrackingsdk.data.remote.models.requests
-
-data class RefreshTokenRequest(val refreshToken: String)
